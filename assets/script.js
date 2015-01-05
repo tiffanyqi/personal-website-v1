35e991c51af1370ab993ec015b98b06613894b97
Tiffany Qi <tiffany19qi@berkeley.edu>
2015-01-04 23:20:30 -0800

    added graphics, content

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("#title a, #navbar a").click(function() {
         var topHeight = $("#title").height() + $("#navbar").height()
         $("html, body").animate({
-            scrollTop: $(this.hash).offset().top - topHeight - 56
+            scrollTop: $(this.hash).offset().top - topHeight
         });
     });
 });
