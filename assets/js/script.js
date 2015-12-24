05443a73fddb444c2f65405b017408d4c2b08912
Tiffany Qi <tiffany19qi@berkeley.edu>
2015-12-24 00:01:45 -0800

    trying to open d3

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,20 +7,6 @@ $(document).ready(function() {
     //         scrollTop: $(this.hash).offset().top - topHeight
     //     });
     // });
-
-
-    // http://www.abeautifulsite.net/smoothly-scroll-to-an-element-without-a-jquery-plugin-2/
-    $('a[href^="#"]').on('click', function(event) {
-
-        var target = $( $(this).attr('href') );
-
-        if( target.length ) {
-            event.preventDefault();
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 700);
-        }
-    });
   
 
     // Navbar color stays if clicked, then removed
