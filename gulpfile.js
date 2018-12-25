a6b9079171a81737f5d17b14ff328dad0017cb04
Tiffany Qi <tiffany19qi@gmail.com>
2018-12-25 11:07:58 -0800

    add base

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ var gulp = require('gulp'),
     jade = require('gulp-jade');
 
 gulp.task('jade', function() {
-gulp.src('*.jade')
-    .pipe(jade())
-    .pipe(gulp.dest('.'))
+    return gulp.src('assets/jade/*')
+        .pipe(jade())
+        .pipe(gulp.dest('.'))
 });
-gulp.watch(['*.jade', 'js/*', 'css/*']);
+gulp.watch(['assets/jade/*', 'assets/js/*', 'assets/css/*']);
