6f7ce741d75fbeac31f0d6530e6eee287b3ed2ff
Tiffany Qi <tiffany19qi@gmail.com>
2018-12-27 11:49:11 -0800

    make charts a bit better

--- assets/js/charts.js
+++ assets/js/charts.js
@@ -5,24 +5,31 @@ google.charts.setOnLoadCallback(drawTimeSpentChart);
 
 const barOptions = {
   backgroundColor: '#c5d3de',
+  bar: {
+    groupWidth: '90%',
+  },
   bars: 'horizontal',
+  height: 400,
+  fontName: 'PT Sans',
+  fontSize: 16,
   legend: {
     position: 'none',
   },
   tooltip: {
     isHtml: true,
   },
+  width: 500,
 }
 
 function drawHumanLanguagesChart() {
   var data = google.visualization.arrayToDataTable([
-    ['', 'Proficiency', {role: 'tooltip'}],
-    ['English', 100, 'Born and raised in the US.'],
-    ['Shanghainese', 70, 'Raised in a Mandarin/Shanghainese bilingual household. Can speak and listen conversationally.'],
-    ['Mandarin Chinese', 60, 'Raised in a Mandarin/Shanghainese bilingual household. Can speak and listen conversationally, and read 40% of characters'],
-    ['French', 25, 'Took 4 years in high school and 1 semester in college. Still remembers how to conjugate, speak, and read.'],
-    ['Japanese', 20, 'Self taught Genki 1. Can read hiragana, katakana, and 15% kanji, speaking and listening skills lacking. Could pass the JLPT N5.'],
-    ['Korean', 10, 'Self taught. Can read characters, rudimentary knowledge of grammar and knows some words.'],
+    ['', 'Proficiency', {role: 'tooltip'}, {role: 'style'}],
+    ['English', 100, '100% Proficiency - Born and raised in the US.', '#778fb5'],
+    ['Shanghainese', 70, '70% Proficiency - Raised in a Mandarin/Shanghainese bilingual household. Can speak and listen conversationally.', '#e18f77'],
+    ['Mandarin Chinese', 60, '60% Proficiency - Raised in a Mandarin/Shanghainese bilingual household and took 1.5 years of college advanced Chinese. Can speak and listen conversationally, and read 40% of characters.', '#e18f77'],
+    ['French', 25, '25% Proficiency - Took 4 years in high school and 1 semester in college. Still remembers how to conjugate, speak, and read.', '#778fb5'],
+    ['Japanese', 20, '20% Proficiency - Self taught Genki 1. Can read hiragana, katakana, and 15% kanji, speaking and listening skills lacking. Could pass the JLPT N5.', '#e18f77'],
+    ['Korean', 10, '10% Proficiency - Self taught. Can read characters, rudimentary knowledge of grammar and knows some words.', '#e18f77'],
   ]);
   var chart = new google.visualization.BarChart(document.getElementById('human-languages-proficiency'));
   chart.draw(data, barOptions);
@@ -30,15 +37,17 @@ function drawHumanLanguagesChart() {
 
 function drawProgrammingLanguagesChart() {
   var data = google.visualization.arrayToDataTable([
-    ['', 'Proficiency'],
-    ['Javascript', 70],
-    ['PanelJS', 60],
-    ['AngularJS' , 30],
-    ['Jade/HTML', 85],
-    ['CSS/Sass', 85],
-    ['Python', 60],
-    ['Ruby', 40],
-    ['Java', 20],
+    ['', 'Proficiency', {role: 'tooltip'}, {role: 'style'}],
+    ['HTML', 85, '85% Proficiency - 4 years, used at Mixpanel.', '#778fb5'],
+    ['CSS/Sass', 85, '85% Proficiency - 4 years, used at Mixpanel.', '#778fb5'],
+    ['Javascript', 70, '70% Proficiency - 2 years, most used language at Mixpanel. Can use it without breaking anything.', '#778fb5'],
+    ['Python', 70, '70% Proficiency - 3 years, used at Mixpanel, 3 side projects, and my default back-end language.', '#e18f77'],
+    ['Django', 70, '70% Proficiency - 2 years, used at Mixpanel, 3 side projects, and my default web app framework. Can create a working web app from start to finish.', '#e18f77'],
+    ['Jade', 60, '60% Proficiency - 2 years, used at Mixpanel.', '#778fb5'],
+    ['PanelJS', 60, '60% Proficiency - 1 year, primary JS framework at Mixpanel. Can create components and use it without breaking anything.', '#778fb5'],
+    ['Ruby', 50, '50% Proficiency - 1 year, one class and side project. Knows syntax and can use it as a back-end language.', '#e18f77'],
+    ['Java', 50, '50% Proficiency - 1 year, one class and side project. Knows syntax and can use it as a back-end language.', '#e18f77'],
+    ['AngularJS' , 40, '40% Proficiency - 1/2 year, one side project. Can work with it.', '#778fb5'],
   ]);
   var chart = new google.visualization.BarChart(document.getElementById('programming-languages-proficiency'));
   chart.draw(data, barOptions);
