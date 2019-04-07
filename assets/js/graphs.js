df4d47fe01f48824f4baebcba84c85833493535f
Tiffany Qi <tiffany19qi@gmail.com>
2019-04-07 17:25:26 +0800

    refactor graphs

--- assets/js/graphs.js
+++ assets/js/graphs.js
@@ -1,99 +1,87 @@
-/*
-  Column Chart
-  * {String} url: source of data
-  * {String} query: query for the graph. looks like this: SELECT A,H,I,J,N,S,T, where the first letter is the column label
-  * {String} title: title of the graph
-  * {String} hAxisTitle: title of the horzontal axis
-  * {String} vAxisTitle: title of the vertical axis
-  * {String} isStacked: true/false
-  * {String} id: id of the graph to be displayed
-*/
+/* Parameters that apply to all chart types
+ * {String} containerId: id of the graph to be displayed
+ * {String} dataSourceUrl: source of data
+ * {Object} options: options to control the graph
+ * {String} query: query for the graph. looks like this: SELECT A,H,F, where the first letter is the column label
+ */
+function drawChart({chartType, containerId, dataSourceUrl, query, options}) {
+  const wrapper = new google.visualization.ChartWrapper(arguments[0]);
+  wrapper.draw();
+}
+
+/* Parameters that apply to all options
+ * {String} hAxisTitle: title of the horzontal axis
+ * {String} title: title of the graph
+ * {String} vAxisTitle: title of the vertical axis
+ */
+function formatOptions({hAxisTitle=null, title, vAxisTitle=null, specialChartOptions=null}) {
+  return {
+    ...specialChartOptions,
+    'hAxis': {'title': hAxisTitle},
+    title,
+    'vAxis': {'title': vAxisTitle},
+  };
+}
+
+/* Area Chart, extra options
+ * {String} isStacked: stacked options, could be true, false, percent, relative
+ */
+function drawAreaChart({containerId, hAxisTitle=null, isStacked, query, title, vAxisTitle=null, url}) {
+  const options = formatOptions({hAxisTitle, title, vAxisTitle, specialChartOptions: {
+    isStacked,
+  }});
+  drawChart({
+    ...arguments[0],
+    chartType: 'AreaChart',
+    dataSourceUrl: url,
+    options,
+  });
+}
+
+/* Column Chart, extra options
+ * {String} isStacked: stacked options, could be true, false, percent, relative
+ */
 function drawColumnChart({containerId, hAxisTitle=null, isStacked, query, title, url, vAxisTitle=null}) {
-  var wrapper = new google.visualization.ChartWrapper({
+  const options = formatOptions({hAxisTitle, title, vAxisTitle, specialChartOptions: {
+    'bar': {'groupWidth': '50%'},
+    'isStacked': isStacked,
+  }});
+  drawChart({
+    ...arguments[0],
     chartType: 'ColumnChart',
     dataSourceUrl: url,
-    query,
-    options: {
-      'title': title,
-      'hAxis': {'title': hAxisTitle},
-      'vAxis': {'title': vAxisTitle},
-      'bar': {'groupWidth': '50%'},
-      'isStacked': isStacked,
+    options,
+  });
+}
+
+/* Line Chart
+ */
+function drawLineChart({containerId, hAxisTitle=null, query, title, vAxisTitle=null, url}) {
+  const options = formatOptions({hAxisTitle, title, vAxisTitle, specialChartOptions: {
+    'curveType': 'function'
+  }});
+  drawChart({
+    ...arguments[0],
+    chartType: 'LineChart',
+    dataSourceUrl: url,
+    options,
+  });
+}
+
+/* Scatter Chart, extra options
+ * {String} showR2: whether to display the r2 value, true/false
+ * {String} visibleInLegend: whether to display the r2 value in the legend, true/false
+ */
+function drawScatterChart({containerId, hAxisTitle=null, query, showR2='true', title, vAxisTitle=null, visibleInLegend='true', url}) {
+  const options = formatOptions({hAxisTitle, title, vAxisTitle, specialChartOptions: {
+    'trendlines': {
+      '0': {showR2, visibleInLegend},
     },
-    containerId,
-  });
-  wrapper.draw();
-}
-
-// line chart
-function drawSemVCumulative() {
-  var wrapper = new google.visualization.ChartWrapper({
-    chartType: 'Line',
-    dataSourceUrl: 'https://docs.google.com/spreadsheets/d/18hPB3_g7A_tC4RAvK6LbLyZYf8tilpum0lSdysCawzI/edit#gid=0',
-    query: 'SELECT A,G,F',
-    containerId: 'SemVCumulative'
-  });
-  wrapper.draw();
-}
-
-// scatter chart
-function drawGradeVClass() {
-  var wrapper = new google.visualization.ChartWrapper({
+  }});
+  drawChart({
+    ...arguments[0],
     chartType: 'ScatterChart',
-    dataSourceUrl: 'https://docs.google.com/spreadsheets/d/1Lj1mwa9xA3SgPuOISCVY_TReWmf0ecMpHj31NxQRE4s/edit#gid=0',
-    query: 'SELECT E,J',
-    options: {
-      'title': 'Grade Received vs. Time Spent Going to Class',
-      'hAxis': {'title': 'Grade Received (GPA)'},
-      'vAxis': {'title': 'Time Spent (hours)'},
-      'trendlines': {
-        '0': {
-          'showR2': 'true',
-          'visibleInLegend': 'true'
-        }
-      },
-    },
-    containerId: 'GradeVClass'
+    dataSourceUrl: url,
+    options,
   });
-  wrapper.draw()
-}
-
-// bubble chart
-function drawGradeVFinalGrade() {
-  var wrapper = new google.visualization.ChartWrapper({
-    chartType: 'BubbleChart',
-    dataSourceUrl: 'https://docs.google.com/spreadsheets/d/1Lj1mwa9xA3SgPuOISCVY_TReWmf0ecMpHj31NxQRE4s/edit#gid=0',
-    query: 'SELECT A,E,R,T',
-    options: {
-      'title': 'Grade Received vs. Final Exam Grade w/ % Weights',
-      'hAxis': {'title': 'Grade Received (GPA)'},
-      'vAxis': {'title': 'Final Exam Grade (%)'},
-      'bubble': {'textStyle': {'color': 'transparent'}},
-      'colorAxis': 'null' // still looks weird
-    },
-    containerId: 'GradeVFinalGrade'
-  });
-  wrapper.draw()
-}
-
-function drawGradeVPercent() {
-  var wrapper = new google.visualization.ChartWrapper({
-    chartType: 'ScatterChart',
-    dataSourceUrl: 'https://docs.google.com/spreadsheets/d/1Lj1mwa9xA3SgPuOISCVY_TReWmf0ecMpHj31NxQRE4s/edit#gid=0',
-    query: 'SELECT E,H,V',
-    options: {
-      'title': 'Grade Received vs. Percentage Received',
-      'hAxis': {'title': 'Grade Received (GPA)'},
-      'vAxis': {'title': 'Percentage (%)'},
-      'trendlines': {
-        '0': {
-        },
-        '1': {
-          'lineWidth': 30,
-        }
-      },
-    },
-    containerId: 'GradeVPercent'
-  });
-  wrapper.draw()
 }
